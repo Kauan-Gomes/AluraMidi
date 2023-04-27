71ed5f53c72bc21d84ca95fb59e7e64233971b87
Kauan Gomes <Kauang13@gmail.com>
2023-04-27 14:45:28 -0300

    Ajustando som

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 
 // Som Pom 
-function tocaSom(idElementoAudio){
-    
-    
+function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
 
 const ListaDeteclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < ListaDeteclas.length){
+for (let contador = 0; contador < ListaDeteclas.length; contador++) {
 
     const tecla = ListaDeteclas[contador];
     const instrumento = tecla.classList[1];
 
-    const idAudio = `#som_${instrumento}`
-    
-    
+    const idAudio = `#som_${instrumento}`;
+
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    
-    contador = contador + 1;
 
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code)
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
 
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
